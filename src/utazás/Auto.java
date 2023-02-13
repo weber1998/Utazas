21504998621ab36421ee5f4574ae763a6a04a835
weber1998 <krsz1998182000@gmail.com>
2023-02-13 09:41:59 +0100

    diagram

---
+++ src/utazás/Auto.java
@@ -0,0 +1,16 @@
+
+package utazás;
+
+public class Auto extends Jarmu{
+    
+    private boolean defekt;
+    
+    public void kereketCserel() {
+        
+    }
+    
+    public boolean halad() {
+        return false;
+    }
+    
+}
---
+++ src/utazás/Jarmu.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utazás;
+
+/**
+ *
+ * @author krisz
+ */
+public abstract class Jarmu {
+    private boolean beinditva;
+    private boolean uzemanyag;
+    private boolean megerkezett;
+    
+    public void beindit() {
+        
+    }
+    
+    public void leallit() {
+        
+    }
+    
+    public boolean tankol() {
+        return false;
+    }
+    
+    public boolean halad;
+}
